d808a7da2cc5ada367c53f6162c57c2233526954
achiewe <mamukashviliachi706@gmail.com>
2023-05-02 19:43:57 +0400

    create project and config some files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+function App(): JSX.Element {
+  return <Main> </Main>;
+}
+
+const Main = styled.main`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f8e9dd;
+`;
+export default App;
